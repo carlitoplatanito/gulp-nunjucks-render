31f6c6d8b07c6119ff0176f126a6b9b4ab13a6a1
Ben Daley <benjamindaley@gmail.com>
2015-02-24 15:06:40 +1100

    Normalize whitespace

--- index.js
+++ index.js
@@ -5,36 +5,36 @@ var through = require('through2');
 var nunjucks = require('nunjucks');
 
 module.exports = function (options) {
-	options = options || {};
+    options = options || {};
 
-	return through.obj(function (file, enc, cb) {
-		if (file.isNull()) {
-			this.push(file);
-			return cb();
-		}
+    return through.obj(function (file, enc, cb) {
+        if (file.isNull()) {
+            this.push(file);
+            return cb();
+        }
 
-		if (file.data) {
+        if (file.data) {
             options = _.merge(file.data, options);
-		}
+        }
 
-		if (file.isStream()) {
-			this.emit('error', new gutil.PluginError('gulp-nunjucks', 'Streaming not supported'));
-			return cb();
-		}
+        if (file.isStream()) {
+            this.emit('error', new gutil.PluginError('gulp-nunjucks', 'Streaming not supported'));
+            return cb();
+        }
 
-		
-		options.name = typeof options.name === 'function' && options.name(file) || file.relative;
-		var _this = this;
-		nunjucks.renderString(file.contents.toString(), options, function (err, result) {
-			if (err) {
-				_this.emit('error', new gutil.PluginError('gulp-nunjucks', err));
-			}
-			file.contents = new Buffer(result);
-			file.path = gutil.replaceExtension(file.path, '.html');
-			_this.push(file);
-			cb();
-		});
-	});
+
+        options.name = typeof options.name === 'function' && options.name(file) || file.relative;
+        var _this = this;
+        nunjucks.renderString(file.contents.toString(), options, function (err, result) {
+            if (err) {
+                _this.emit('error', new gutil.PluginError('gulp-nunjucks', err));
+            }
+            file.contents = new Buffer(result);
+            file.path = gutil.replaceExtension(file.path, '.html');
+            _this.push(file);
+            cb();
+        });
+    });
 };
 
 module.exports.nunjucks = nunjucks;
