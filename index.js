fb189526034279ae585985421ce71bc9a14d26e2
Ben Daley <benjamindaley@gmail.com>
2015-02-24 15:35:09 +1100

    Stop variable bleed between templates

--- index.js
+++ index.js
@@ -8,13 +8,16 @@ module.exports = function (options) {
     options = options || {};
 
     return through.obj(function (file, enc, cb) {
+
+        var data = _.extend(options, {});
+
         if (file.isNull()) {
             this.push(file);
             return cb();
         }
 
         if (file.data) {
-            options = _.merge(file.data, options);
+            data = _.merge(file.data, data);
         }
 
         if (file.isStream()) {
@@ -25,7 +28,7 @@ module.exports = function (options) {
 
         options.name = typeof options.name === 'function' && options.name(file) || file.relative;
         var _this = this;
-        nunjucks.renderString(file.contents.toString(), options, function (err, result) {
+        nunjucks.renderString(file.contents.toString(), data, function (err, result) {
             if (err) {
                 _this.emit('error', new gutil.PluginError('gulp-nunjucks', err));
             }
